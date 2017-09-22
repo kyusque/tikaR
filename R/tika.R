b739800c127234bf33e4f58950e5b15432652662
kyusque <kawashimayusuke113@gmail.com>
2017-09-22 23:24:17 +0900

    initial commit

---
+++ R/tika.R
@@ -0,0 +1,27 @@
+
+#' Apache tika parser to xml in R
+#'
+#' @param file file path
+#' @importFrom rJava .jinit
+#' @importFrom rJava .jaddClassPath
+#' @importFrom rJava .jnew
+#' @return xml
+#' @export
+#'
+#' @examples
+#' tika_parse_to_xml("presentation.pptx")
+#'
+
+tika_parse_to_xml <- function(file){
+    .jinit()
+    .jaddClassPath("tika-app-1.16.jar")
+    fileIO <- .jnew("java.io.FileInputStream",file)
+    parser <- .jnew("org.apache.tika.parser.AutoDetectParser")
+    handler <- .jnew("org.apache.tika.sax.ToXMLContentHandler")
+    metadata <- .jnew("org.apache.tika.metadata.Metadata")
+    parser$parse(fileIO,handler,metadata)
+
+    result <- handler$toString()
+    return(iconv(result,"UTF-8","UTF-8")) # こうしないと文字化けする(Windows)。謎。 //逕ｻ髱｢縺ｫ蜷医ｏ縺帙ｋ (4:3) => 画面に合わせる (4:3)
+
+}
